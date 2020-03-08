4a2b15a3e5a019eee11cc0004d3b2d5a59f44d82
UchiTesting <uchitesting57@gmail.com>
2020-03-08 16:52:49 +0100

    Ex. 3 Initial Commit.

---
+++ 200306-Exo03 BossMonster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _200306_Exo03_BossMonster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex. 3 BossMonster");
+        }
+    }
+}
