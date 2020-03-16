394eb66fcf967864990eaffa013f7593513972d3
UchiTesting <uchitesting57@gmail.com>
2020-03-16 22:03:51 +0100

    Adding exercice 5 in a functional state.

---
+++ 200316-Exo05 Chest Event/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static Simple_IO.AskData;
+
+namespace _200316_Exo05_Chest_Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ex 05: Chest Event");
+
+            Random random = new Random();
+
+            Chest myChest = new Chest(random, 5);
+            ChestListener myChestListener = new ChestListener(myChest);
+
+            do
+            {
+                int attempt = askInt("Please input a code to try: ");
+                myChest.TryCode(attempt);
+            } while (!myChest.getLockState() && !myChest.getFoundState());
+        }
+    }
+}
