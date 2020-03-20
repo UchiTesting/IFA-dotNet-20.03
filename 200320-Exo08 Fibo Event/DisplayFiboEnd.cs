adb1ac1ad670d51cb92afd60846984ca6abd6a6b
UchiTesting <uchitesting57@gmail.com>
2020-03-20 13:57:05 +0100

    Exercice 8 finished.

---
+++ 200320-Exo08 Fibo Event/DisplayFiboEnd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _200320_Exo08_Fibo_Event
+{
+    public class DisplayFiboEnd
+    {
+        public static void OnThreadFinishedEvent(object sender, FiboArgs fa)
+        {
+            DateTime timestamp = DateTime.Now;
+
+            Console.WriteLine("{0}: Finished to compute Fibo for N={1}. Result: {2}", timestamp, fa.n,fa.result);
+        }
+    }
+}
---
+++ 200320-Exo08 Fibo Event/FiboArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace _200320_Exo08_Fibo_Event
+{
+    public class FiboArgs
+    {
+        public int n;
+        public BigInteger result;
+    }
+}
